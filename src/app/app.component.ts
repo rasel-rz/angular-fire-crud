f5b012c29d43776fad08028e298e5b6a8e3eb0a3
rasel-rz <md.raihanbdrasel@gmail.com>
2020-05-31 23:27:23 +0600

    added to gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'OnlineStoreApp';
+  title = "OnlineStoreApp";
 }
+
+// The command for production
+// ng build --prod --output-path docs --base-href /angular-fire-crud/
