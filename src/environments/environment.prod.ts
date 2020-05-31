867fdf091835c96ecf95f77abe340f7c0ad5ab4e
rasel-rz <md.raihanbdrasel@gmail.com>
2020-05-31 23:41:49 +0600

    zzz

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "******************************",
+    authDomain: "myawesomeonlinestore.firebaseapp.com",
+    databaseURL: "https://myawesomeonlinestore.firebaseio.com",
+    projectId: "myawesomeonlinestore",
+    storageBucket: "myawesomeonlinestore.appspot.com",
+    messagingSenderId: "*****************",
+    appId: "*********************************",
+    measurementId: "**********************",
+  },
+};
